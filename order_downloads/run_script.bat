9f010effd255f0a2bdedd2bc71d1bb197214e8cf
Chao Alarcón <chaoalarcon03@gmail.com>
2026-03-12 17:58:10 +0100

    Adaptación de archivos y comandos a Chao-Laptop

--- order_downloads/run_script.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-python "D:\GitHub Repositories\custom-automation-functions\order_downloads\main.py"
-python "D:\GitHub Repositories\custom-automation-functions\order_screenshots\main.py"
-
-exit
--- run_script.bat
+++ run_script.bat
@@ -1,6 +1,7 @@
 @echo off
-cd /d "D:\GitHub Repositories\custom-automation-functions\order_downloads"
+
+cd /d "C:\Customizacion_del_Sistema\custom-automation-functions\order_downloads"
 python main.py
 
-cd /d "D:\GitHub Repositories\custom-automation-functions\order_screenshots\"
+cd /d "C:\Customizacion_del_Sistema\custom-automation-functions\order_screenshots"
 python main.py
